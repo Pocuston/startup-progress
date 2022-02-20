1510e216182bbde15a5011fbc8b6f120cd787981
Jakub Šlambora <jakub.slambora@unicorn.com>
2022-02-20 15:57:36 +0100

    Load and display random fact

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import * as React from "react";
+import { useEffect, useState } from "react";
 import Container from "@mui/material/Container";
 import ProgressView from "./ProgressView";
 import useStartupProgressData from "./useStartupProgressData";
+import Alert from "@mui/material/Alert";
 
 export default function App() {
   const [startupProgressData, handleStepCompleteChange] =
     useStartupProgressData();
+  const [randomFact, setRandomFact] = useState<string | null>(null);
+  const [randomFactDisplayed, setRandomFactDisplayed] = useState(false);
+
+  useEffect(() => {
+    if (randomFactDisplayed || !areAllStagesCompleted()) {
+      return;
+    }
+
+    async function fetchRandomFact() {
+      const response = await fetch("https://uselessfacts.jsph.pl/random.json");
+      const data = await response.json();
+      setRandomFact(data.text);
+      setRandomFactDisplayed(true);
+    }
+    fetchRandomFact();
+  }, [randomFactDisplayed, areAllStagesCompleted()]);
+
+  function areAllStagesCompleted() {
+    return startupProgressData.stages.every((stage) => stage.completed);
+  }
 
   return (
     <Container maxWidth="sm">
@@ -15,6 +37,7 @@ export default function App() {
           onStepComplete={handleStepCompleteChange}
         />
       )}
+      {randomFact !== null && <Alert>{randomFact}</Alert>}
     </Container>
   );
 }
